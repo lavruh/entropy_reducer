789d662dd66c0f4ccc114123524c0d8e5ca8d4a4
FlutLab.io <>
2021-10-11 21:05:36 +0000

    Autosave

--- lib/ui/screens/tags_editor_screen.dart
+++ lib/ui/screens/tags_editor_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:entropy_reducer/ui/widgets/tags_widget.dart';
+import 'package:entropy_reducer/domian/states/tags_state.dart';
 
 class TagsEditor extends StatelessWidget {
+  final _ctrl = Get.put<TagsState>(TagsState(Get.find()), permanent: true);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
